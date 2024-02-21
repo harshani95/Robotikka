ab6de6803c9915725daafff51c13f67c7189dc59
harshani <ruwanthikapemarathna@gmail.com>
2024-02-21 10:51:24 +0530

    added Database Access Code

---
+++ src/com/devstack/pos/dao/DatabaseAccessCode.java
@@ -0,0 +1,47 @@
+package com.devstack.pos.dao;
+
+import com.devstack.pos.dto.UserDto;
+import com.devstack.pos.util.PasswordManager;
+
+import java.sql.*;
+
+public class DatabaseAccessCode {
+
+    //========user management==========//
+    public static boolean createUser(
+            String email,
+            String password
+    ) throws ClassNotFoundException, SQLException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka", "root", "1218");
+        String sql = "INSERT INTO user VALUES (?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, email);
+        preparedStatement.setString(2, PasswordManager.encryptPassword(password));
+        return preparedStatement.executeUpdate()>0;
+    }
+
+    public static UserDto findUser(String email) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka", "root", "1218");
+        String sql = "SELECT * FROM user WHERE email=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, email);
+
+        ResultSet set = preparedStatement.executeQuery();
+        if (set.next()) {
+            return new UserDto(
+                    set.getString(1),
+                    set.getString(2));
+        }
+        return null;
+    }
+    //========user management==========//
+
+
+    //========customer management==========//
+   // public static boolean createCustomer(){}
+
+    //========customer management==========//
+}
