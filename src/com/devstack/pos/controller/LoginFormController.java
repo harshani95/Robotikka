5c6bcf168816e999c50e07d817f89a78d5942429
harshani <ruwanthikapemarathna@gmail.com>
2024-02-21 10:48:32 +0530

    added Login Form Controller

---
+++ src/com/devstack/pos/controller/LoginFormController.java
@@ -0,0 +1,57 @@
+package com.devstack.pos.controller;
+
+import com.devstack.pos.dao.DatabaseAccessCode;
+import com.devstack.pos.dto.UserDto;
+import com.devstack.pos.util.PasswordManager;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.*;
+
+public class LoginFormController {
+    public JFXPasswordField txtPassword;
+    public JFXTextField txtEmail;
+    public AnchorPane context;
+
+
+    public void btnSignInOnAction(ActionEvent actionEvent) {
+        try {
+            UserDto ud = DatabaseAccessCode.findUser(txtEmail.getText());
+            if (ud!=null) {
+                if (PasswordManager.checkPassword(txtPassword.getText(), ud.getPassword()) ){
+                   setUi("DashboardForm");
+                } else {
+                    new Alert(Alert.AlertType.WARNING, "Check your password try again!").show();
+                }
+            } else {
+                new Alert(Alert.AlertType.WARNING, "User Email not found!").show();
+            }
+        } catch (ClassNotFoundException | SQLException | IOException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+    }
+
+
+    public void btnCreateAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+            setUi("SignUpForm");
+    }
+
+        public void setUi(String url) throws IOException {
+            Stage stage = (Stage)context.getScene().getWindow();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+url+".fxml"))));
+            stage.centerOnScreen();
+
+        }
+}
+
+
+
+
