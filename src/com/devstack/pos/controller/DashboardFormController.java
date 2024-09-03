aeae769bbf52c701b11ac15dd3b266db32c4646e
harshani <ruwanthikapemarathna@gmail.com>
2024-09-03 14:37:18 +0530

    Created Dashboard

--- src/com/devstack/pos/controller/DashboardFormController.java
+++ src/com/devstack/pos/controller/DashboardFormController.java
@@ -1,15 +1,22 @@
 package com.devstack.pos.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardFormController {
     public AnchorPane context;
 
-    public void btnCustomerManagementOnAction(ActionEvent actionEvent) {
+    public void btnCustomerManagementOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("CustomerForm");
     }
 
-    public void btnProductManagementOnAction(ActionEvent actionEvent) {
+    public void btnProductManagementOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("ProductForm");
     }
 
     public void btnOrderDetailsOnAction(ActionEvent actionEvent) {
@@ -19,5 +26,13 @@ public class DashboardFormController {
     }
 
     public void btnPlaceOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    private void setUi(String url) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.centerOnScreen();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/" + url + ".fxml")))
+        );
     }
 }
